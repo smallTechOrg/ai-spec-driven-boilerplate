cdfc5e82346462c7f6c98dea6ad7c3b04310cc64
Sai Krishna Vennamaneni <saivnm5@gmail.com>
2026-04-24 00:43:19 +0530

    chore: remove stray src/bloggen/ (leftover from a prior build attempt, swept in by git add -A)

--- src/bloggen/src/bloggen/__main__.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-
-from bloggen.api import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    uvicorn.run("bloggen.__main__:app", host="0.0.0.0", port=8001, reload=True)
